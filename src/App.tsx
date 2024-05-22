b0228d383cfb238947097bd662e02ba293fb82e7
mulinatti <gabrielmulinari2002@gmail.com>
2024-05-22 20:55:28 -0300

    initial commit - node modules included no internet

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button>Cadastrar</Button>
+    </>
+  )
+}
+
+export default App;
