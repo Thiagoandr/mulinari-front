8de3163f082fc33c8c7141396c42b06893a25887
andradet <andradet@pge.rj.gov.br>
2024-05-30 12:54:47 -0300

    card 1 e card 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import './App.css'
-import { Button } from './components/ui/button'
 
-function App() {
 
+import React from 'react';
+import './App.css';
+import { Card } from './components/ui/card';
+
+const App: React.FC = () => {
   return (
-    <>
-      <Button>Cadastrar</Button>
-    </>
-  )
+    <div className="card-container">
+      <div className="card">
+        <Card className="card-1">
+        
+        </Card>
+      </div>
+      <div className="card">
+        <Card className="card-2">
+        
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export default App;
