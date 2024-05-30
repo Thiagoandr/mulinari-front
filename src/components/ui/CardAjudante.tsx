d7a1bf17eeb7c8efca1e39d8c3e3aaacd08d59e6
andradet <andradet@pge.rj.gov.br>
2024-05-30 19:42:06 -0300

    Fix: tela principal

---
+++ src/components/ui/CardAjudante.tsx
@@ -0,0 +1,24 @@
+import { DollarSign, Truck } from "lucide-react";
+import { Card, CardContent, CardTitle } from "./card";
+
+const CardAjudante = () => {
+
+    return (
+        <Card className="flex justify-between items-center p-8 hover:shadow-lg hover:cursor-pointer">
+          <CardTitle className="ml-4">Negueba</CardTitle>
+
+          <CardContent className="flex gap-10 items-center p-0 mr-4">
+            <div className="flex gap-2 items-center">
+              <Truck size={30} className="text-zinc-500/90"></Truck>
+              <span className="text-xl font-semibold">2</span>
+            </div>
+            <div className="flex gap-2 items-center">
+              <DollarSign size={25} className="text-red-500" />
+              <span className="text-xl font-semibold">180,00</span>
+            </div>
+          </CardContent>
+        </Card>
+    )
+}
+
+export default CardAjudante;
