e7b30e3ebacf572d10e485f6d420cea6c089fda3
mulinatti <gabrielmulinari2002@gmail.com>
2024-06-02 19:42:57 -0300

    add: formulario cadastro ajudante

--- src/components/CadastroAjudante.tsx
+++ src/components/CadastroAjudante.tsx
@@ -1,7 +1,31 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input"
+
 const CadastroAjudante = () => {
+//adicionar checkbox de motorista
   return (
-    <main>CadastroAjudante</main>
+    <main className="w-2/4 mx-auto space-y-6">
+      <section className="space-y-6">
+        <div className="font-medium">
+          <label>Nome</label>
+          <Input/>
+        </div>
+        <div className="font-medium">
+          <label>Apelido</label>
+          <Input/>
+        </div>
+        <div className="font-medium">
+          <label>Data de nascimento</label>
+          <Input/>
+        </div>
+        <div className="font-medium">
+          <label>Telefone</label>
+          <Input/>
+        </div>
+      </section>
+      <Button>Cadastrar</Button>
+    </main>
   )
 }
 
-export default CadastroAjudante
+export default CadastroAjudante;
