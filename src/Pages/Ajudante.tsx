60cd30551c45caf980b48fffe234fef222c937cb
andradet <andradet@pge.rj.gov.br>
2024-06-02 19:42:19 -0300

    card serviço

--- src/Pages/Ajudante.tsx
+++ src/Pages/Ajudante.tsx
@@ -4,25 +4,26 @@ import { CardTitle } from "@/components/ui/card";
 const Ajudante = () => {
   return (
     <div>
-      <div className="flex items-center">
-        <img
-          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
-          alt="Ajudante"
-          className="h-48 w-48 mr-5" 
-        />
-        <div className="ml-4">
-          <CardTitle className="text-5xl">Negueba</CardTitle>
-          <br />
-          <span className="ml-0 text-x text-zinc-500">Edgar Bivara</span>
-        </div>
-      </div>
-     
-        <CardServiço/>
-        <CardServiço/>
-        <CardServiço/>
-        <CardServiço/>
-    </div>
+  <div className="flex items-center">
+    <img
+      src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+      alt="Ajudante"
+      className="h-48 w-48 mr-5" 
+    />
+    <div className="ml-4">
+      <CardTitle className="text-5xl">Negueba</CardTitle>
+      <br />
+      <span className="ml-0 text-x text-zinc-500">Edgar Bivara</span>
+      <br />
       
+    </div>
+  </div>
+  <CardServiço/>
+  
+  <CardServiço/>
+  <CardServiço/>
+  <CardServiço/>
+</div>
   );
 };
 
--- src/components/CardServiço.tsx
+++ src/components/CardServiço.tsx
@@ -3,12 +3,20 @@ import { Card, CardContent, CardTitle } from './ui/card'
 import { DollarSign, Truck, User } from 'lucide-react'
 
 const CardServiço = () => {return (
-    <Card  className=" ml-1 flex justify-between items-center p-8 hover:shadow-lg">
-      <CardTitle className="ml-4">Rua dos bobos, Pau-Grande</CardTitle>
-   
+  <Card className="ml-1 flex justify-between items-center p-8 hover:shadow-lg">
+<div>
+  <div className="ml-4">
+    <CardTitle>Rua dos bobos, Pau-Grande</CardTitle>
+    <div className="mt-2">
+      <span className="text-x text-gray-500">Bairro Abelardo Barboza</span>
+      <span className="ml-0 text-gray-400" >-02/10/2023</span> 
+    </div>
+  </div>
+</div>
       <CardContent className="flex gap-10 items-center p-0 mr-4">
     
         <div className="flex gap-2 items-center">
+          
           <User size={30} className="text-zinc-500/90"></User>
 
           <span className="text-xl font-semibold">2</span>
