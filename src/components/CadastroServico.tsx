787c648f19250c0a66cb5047c62c28f91d56cf1e
AfonsoGui08 <guimaraesafonso08@gmail.com>
2024-06-02 20:15:51 -0300

    tela de cadastro de serviço

--- src/components/CadastroServico.tsx
+++ src/components/CadastroServico.tsx
@@ -1,7 +1,70 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 const CadastroServico = () => {
+
+
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+  };
+
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+  };
+
+  const labelStyle = {
+    margin: "10px 0 5px",
+    fontSize: "14px",
+    fontWeight: "bold",  
+  };
+
+  const inputStyle = {
+    margin: "0 0 20px",
+    padding: "10px 5px", 
+    fontSize: "14px",
+    width: "250px",
+  };
+
+  const buttonStyle = {
+    marginTop: "20px",
+    padding: "10px 20px", 
+    fontSize: "14px",
+    fontWeight: "bold",  
+  };
+
   return (
-    <main>CadastroServico</main>
-  )
+     <main className="w-2/4 mx-auto space-y-6">
+      <section className="space-y-6">
+        <div className="font-medium">
+          <label>RUA</label>
+          <Input/>
+        </div>
+        <div className="font-medium">
+          <label>BAIRRO</label>
+          <Input/>
+        </div>
+        <div className="font-medium">
+          <label>DATA</label>
+          <Input/>
+        </div>
+        <div className="font-medium">
+          <label>VALOR DO SERVIÇO</label>
+          <Input/>
+        </div>
+        <div className="font-medium">
+          <label>CAMPO AJUDANTES</label>
+          <Input/>
+        </div>
+      </section>
+      <Button>Cadastrar</Button>
+    </main>
+  );
 }
 
 export default CadastroServico;
