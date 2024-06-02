e7a98f2bf5f99ec6ca25766eda387f315cb63b5c
andradet <andradet@pge.rj.gov.br>
2024-06-02 17:34:37 -0300

    user teste

--- src/Pages/Ajudante.tsx
+++ src/Pages/Ajudante.tsx
@@ -1,16 +1,23 @@
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
-import { DollarSign, Truck } from "lucide-react";
-
-import { useNavigate, useParams } from "react-router-dom";;
+import React from 'react';
 import { User } from 'lucide-react';
 import { CardTitle } from '@/components/ui/card';
 
 const Ajudante = () => {
   return (
-   <div>
-     <h1>Ajudante</h1>
-   </div>
-  )
-}
+    <div className="flex items-center">
+      <img
+        src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        alt="Ajudante"
+        className="h-48 w-48 mr-5" 
+      />
+      <div className="ml-4">
+        <CardTitle className="text-5xl">Negueba</CardTitle>
+        <br></br>
+        <span className=" ml-4 text-xl">teste teste teste</span>
+      </div>
+    </div>
+  );
+};
 
 export default Ajudante;
+
