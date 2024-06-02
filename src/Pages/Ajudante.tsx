29b44ddc59d9847a13879438fef0e7cfddeb1469
andradet <andradet@pge.rj.gov.br>
2024-06-02 17:34:02 -0300

    user teste

--- src/Pages/Ajudante.tsx
+++ src/Pages/Ajudante.tsx
@@ -1,31 +1,16 @@
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { DollarSign, Truck } from "lucide-react";
 
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";;
+import { User } from 'lucide-react';
+import { CardTitle } from '@/components/ui/card';
 
-const CardAjudante = () => {
-
-
-
-
-    const navigate = useNavigate();
-
-    return (
-        <Card onClick = {() => navigate(`/ajudante`)} className="flex justify-between items-center p-8 hover:shadow-lg hover:cursor-pointer">
-          <CardTitle className="ml-4">Negueba</CardTitle>
-
-          <CardContent className="flex gap-10 items-center p-0 mr-4">
-            <div className="flex gap-2 items-center">
-              <Truck size={30} className="text-zinc-500/90"></Truck>
-              <span className="text-xl font-semibold">2</span>
-            </div>
-            <div className="flex gap-2 items-center">
-              <DollarSign size={25} className="text-red-500" />
-              <span className="text-xl font-semibold">180,00</span>
-            </div>
-          </CardContent>
-        </Card>
-    )
+const Ajudante = () => {
+  return (
+   <div>
+     <h1>Ajudante</h1>
+   </div>
+  )
 }
 
-export default CardAjudante;
+export default Ajudante;
