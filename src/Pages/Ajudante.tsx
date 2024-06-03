c5957c179b0e9a38b8ffc7aed611505e141214cf
andradet <andradet@pge.rj.gov.br>
2024-06-02 21:14:33 -0300

    add: tela ajudante completa

--- src/Pages/Ajudante.tsx
+++ src/Pages/Ajudante.tsx
@@ -1,29 +1,38 @@
 import CardServiço from "@/components/CardServiço";
+import { Button } from "@/components/ui/button";
 import { CardTitle } from "@/components/ui/card";
+import { PencilIcon, TrashIcon } from "lucide-react";
 
 const Ajudante = () => {
   return (
-    <div>
-  <div className="flex items-center">
-    <img
-      src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
-      alt="Ajudante"
-      className="h-48 w-48 mr-5" 
-    />
-    <div className="ml-4">
-      <CardTitle className="text-5xl">Negueba</CardTitle>
-      <br />
-      <span className="ml-0 text-x text-zinc-500">Edgar Bivara</span>
-      <br />
-      
-    </div>
-  </div>
-  <CardServiço/>
-  
-  <CardServiço/>
-  <CardServiço/>
-  <CardServiço/>
-</div>
+    <main className="w-3/4 mx-auto">
+      <section className=" ml-4 flex items-center ">
+        <img
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+          alt="Ajudante"
+          className="h-48 w-48 mr-5"
+        />
+        <div className="ml-4 ">
+          <CardTitle className="text-5xl mb-2">Negueba</CardTitle>
+
+          <span className="ml-0 text-x text-zinc-500">Edgar Bivara</span>
+        </div>
+        <div className="flex ml-auto gap-2 mr-8">
+          <Button className=" bg-blue-600 w-[50px] hover:bg-blue-700">
+            <PencilIcon />
+          </Button>
+          <Button variant={"destructive"} className="w-[50px]">
+            <TrashIcon />
+          </Button>
+        </div>
+      </section>
+      <section className="mt-14 space-y-4 w-full">
+        <CardServiço />
+        <CardServiço />
+        <CardServiço />
+        <CardServiço />
+      </section>
+    </main>
   );
 };
 
--- src/components/CardServiço.tsx
+++ src/components/CardServiço.tsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import { Card, CardContent, CardTitle } from './ui/card'
-import { DollarSign, Truck, User } from 'lucide-react'
+import React from "react";
+import { Card, CardContent, CardTitle } from "./ui/card";
+import { DollarSign, Truck, User } from "lucide-react";
+import { Button } from "./ui/button";
 
-const CardServiço = () => {return (
-  <Card className="ml-1 flex justify-between items-center p-8 hover:shadow-lg">
-<div>
-  <div className="ml-4">
-    <CardTitle>Rua dos bobos, Pau-Grande</CardTitle>
-    <div className="mt-2">
-      <span className="text-x text-gray-500">Bairro Abelardo Barboza</span>
-      <span className="ml-0 text-gray-400" >-02/10/2023</span> 
-    </div>
-  </div>
-</div>
+const CardServiço = () => {
+  return (
+    <Card className="ml-1 flex justify-between items-center p-8 hover:shadow-lg hover:scale-[1.01] transition-transform cursor-pointer ">
+      <div>
+        <div className="ml-4">
+          <CardTitle>Rua dos bobos, Pau-Grande</CardTitle>
+          <div className="items-center gap-1 flex mt-2">
+            <span className="text-x text-gray-500 font-italic">
+              Abelardo Barboza
+            </span>
+            <p className="text sm ml-0 text-gray-400 italic">- 02/10/2023</p>
+          </div>
+        </div>
+      </div>
+
       <CardContent className="flex gap-10 items-center p-0 mr-4">
-    
         <div className="flex gap-2 items-center">
-          
           <User size={30} className="text-zinc-500/90"></User>
 
           <span className="text-xl font-semibold">2</span>
@@ -26,9 +29,8 @@ const CardServiço = () => {return (
           <span className="text-xl font-semibold">180,00</span>
         </div>
       </CardContent>
-      
     </Card>
-)
-}
+  );
+};
 
-export default CardServiço
+export default CardServiço;
